1407dcfc7ff42f0875fd216e232d5729f1e7528b
Krishnakantrao <165270531+Krishnakantrao@users.noreply.github.com>
2024-05-13 16:31:11 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform-batch-26"
+    bucket = "mydev-tf-state-bucket-project-terraform-batch-krishna"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
